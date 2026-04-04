9dc9d86d48c517f3d877ecdc3873bff928989154
oluwajohnson <ogunleyejohnson77@gmail.com>
2026-04-04 15:42:32 +0100

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const isAuthenticated = (req, res, next) => {
   if (req.session.user) {
     return next();
   }
-  return res.status(401).json({ message: "Unauthorized" });
+  return res.status(401).json({ message: "You do not have access/ Unauthorized User" });
 };
 
 module.exports = { isAuthenticated };
